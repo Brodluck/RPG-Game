72495950674c30348e138977b6fe4e71ebf1e881
mikanor99 <45227294+mikanor99@users.noreply.github.com>
2018-11-21 19:57:41 +0100

    eliminado código innecesario

--- items.c
+++ items.c
@@ -7,55 +7,47 @@ void itemType(){
 }
 void itemAtbType(item *object){
     int x=rand()%10;
-    if (x<5){
-        strcpy(object->nat, "físico");
-        object->nature=1;
-        if (object->type==2){
-            object->statm=object->stat*0.3;
+    if (x<5){                               //50% probabilidad
+        strcpy(object->nat, "físico");      
+        object->nature=1;                   //asigna naturaleza fisica
+        if (object->type==2){               
+            object->statm=object->stat*0.3; //si es armadura, no elimina por completo la estadistica magica
         }
-    }else {
-        strcpy(object->nat, "mágico");
-        object->nature=2;
+    }else {                                 
+        strcpy(object->nat, "mágico");      
+        object->nature=2;                   //asigna naturaleza magica
         object->statm=object->stat;
-        if (object->type==3){
-            object->stat=object->statm*0.3;
-        }else object->stat=0;
+        if (object->type==2){
+            object->stat=object->statm*0.3; //si es armadura, no elimina por completo la estadistica fisica
+        }else object->stat=0;               //is no lo es, elimina estadistica fisica
     }
 }
-void itemAtbCommon(item *object){
+void itemAtbCommon(item *object){           //atributo comun
     int x=rand()%100;
-    if (x<20){
-        strcpy(object->atr1, "inutil");
-        object->stat=0;
+    if (x<20){                              //20% probabilidad
+        strcpy(object->atr1, "inutil");     
+        object->stat=0;                     //pone stats a 0
         object->statm=0;
-    }else if (x<40){
+    }else if (x<40){                        //20% probabilidad
         strcpy(object->atr1, "imbuido");
-        if (object->type==1 && object->nature==2){
-            object->statm=object->stat*1.2;
-        }else if (object->type==2 && object->nature==2){
-            object->statm=object->stat*1.2;
+        if (object->nature==2){                     //solamente afecta si es un item magico
+            object->statm=object->statm*1.2;
         }
     }else if (x<60){
-        strcpy(object->atr1, "resistente");
-        if (object->type==1 && object->nature==1){
-            object->stat=object->stat*1.2;
-        }else if (object->type==2 && object->nature==1){
+        strcpy(object->atr1, "resistente");         //solamente afecta si es un item fisico
+        if (object->nature==1){
             object->stat=object->stat*1.2;
         }
     }else if (x<80){
-        strcpy(object->atr1, "fragil");
-        if (object->type==1 && object->nature==2){
-            object->stat=object->stat*0.8;
-        } else if (object->type==2 && object->nature==2){
+        strcpy(object->atr1, "fragil");             //solamente afecta si es un item fisico
+        if (object->nature==2){
             object->stat=object->stat*0.8;
         }
     }else{
-        strcpy(object->atr1, "maldito");
-        if (object->type==1 && object->nature==1){
-            object->stat=object->stat*0.8;
-        }else if (object->type==2 && object->nature==1){
-            
-        }           
+        strcpy(object->atr1, "maldito");           //solamente afecta si es un item magico
+        if (object->nature==1){
+            object->statm=object->statm*0.8;
+        }
     }
 }
 void itemAtbRare(item *object){
@@ -64,27 +56,18 @@ void itemAtbRare(item *object){
         
     }else if (x<55){
         strcpy(object->atr1, "encantado");
-        if (object->type==1 && object->nature==2){
-            object->statm=object->statm*1.4;
-        }else if (object->type==2 && object->nature==2){
+        if (object->nature==2){
             object->statm=object->statm*1.4;
         }
     }else if (x<70){
         strcpy(object->atr1, "destructor");
-        if (object->type==1 && object->nature==1){
-            object->stat=object->stat*1.4;
-        }else if (object->type==2 && object->nature==1){
+        if (object->nature==1){
             object->stat=object->stat*1.4;
         }
     }else if (x<85){
         strcpy(object->atr1, "cutre");
-        if (object->type==1){
             object->stat=object->stat*0.7;
             object->statm=object->statm*0.7;
-        } else if (object->type==2){
-            object->stat=object->stat*0.7;
-            object->statm=object->statm*0.7;
-        }
     }
 }
 void itemAtbExtra(item *object){
@@ -92,16 +75,12 @@ void itemAtbExtra(item *object){
     if (x<70){
     }else if (x<80){
         strcpy(object->atr1, "megamagico");
-        if (object->type==1 && object->nature==2){
-            object->statm=object->statm*1.7;
-        }else if (object->type==2 && object->nature==2){
+        if (object->nature==2){
             object->statm=object->statm*1.7;
         }
     }else if (x<90){
         strcpy(object->atr1, "asesino");
-        if (object->type==1 && object->nature==1){
-            object->stat=object->stat*1.7;
-        }else if (object->type==2 && object->nature==1){
+        if (object->nature==1){
             object->stat=object->stat*1.7;
         }
     }
@@ -110,13 +89,8 @@ void itemAtbLegen(item *object){
     int x=rand()%100;
     if (x<93){
         strcpy(object->atr1, "legendario");
-        if (object->type==1){
             object->stat=object->stat*3;
             object->statm=object->statm*3;
-        } else if (object->type==2){
-            object->stat=object->stat*3;
-            object->statm=object->statm*3;
-        }
     }
 }
 void itemGen(item *object, chara *charac) {
