7e6d22b4940169f32bd2a78e3102206f2cb23e3a
guillermo munoz <45227294+mikanor99@users.noreply.github.com>
2018-11-21 20:10:18 +0100

    añadidos comentarios

--- head.h
+++ head.h
@@ -15,7 +15,7 @@
 #define HEAD_H
 typedef struct {
     char name[10];
-    int ID, type, nature;
+    int ID, type, nature; //type (1=armas, 2=armaduras), nature (1=fisico, 2=magico) 
     double stat, statm;
     char atr1[10], atr2[10], atr3[10], atr4[10], nat[10];
 }item;
@@ -27,7 +27,7 @@ typedef struct {
     item equip[5];
 }chara;
 typedef struct {
-    int type, race;                           //atrib
+    int type, race;                     //atrib
     int level;                          //level
     double def, att, defm, attm, vel;   //stats
     item equip[5];
