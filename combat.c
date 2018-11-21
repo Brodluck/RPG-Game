7aa9802fb0b951eb40ad1fd3947811521d619470
Enmanuel <brodluck@yahoo.es>
2018-11-21 18:40:12 +0100

    Primerito_archivo

---
+++ combat.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   combat.c
+ * Author: Enmanuel
+ *
+ * Created on 21 de noviembre de 2018, 18:36
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+int main() {
+
+
+    return 0;
+}
+
