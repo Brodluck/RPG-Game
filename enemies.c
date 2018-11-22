5a826e25fc44aaa3504a6c4fe737c84b5f0019a1
guillermo munoz <45227294+mikanor99@users.noreply.github.com>
2018-11-22 14:44:02 +0100

    añadida generacion de items
    y arreglados algunos bugs

--- enemies.c
+++ enemies.c
@@ -5,8 +5,11 @@
 
 void EnemyGen(chara *charac, npc *enemy) {
     
-    int x=rand()%21, y=rand()%5, i;
+    int x=rand()%21, i;
     enemy->level=charac->level + (x-10);
+    if (enemy->level<0){
+        enemy->level=0;
+    }
     if (enemy->level<10){
         enemy->type=1;
     } else if (enemy->level<20){
@@ -18,39 +21,42 @@ void EnemyGen(chara *charac, npc *enemy) {
     } else if (enemy->level<50){
         enemy->type=5;
     }
-    enemy->race=y;
+    x=(rand()%4)+1;
+    enemy->race=x;
     if (enemy->race==1){              //elfo
     enemy->att = -10;
     enemy->attm = 10;
     enemy->def = -10;
     enemy->defm = 10;
     enemy->vel = 20;
-    printf("seleccionada la raza elfa");
     } else if (enemy->race==2){       //enano
     enemy->att = 10;
     enemy->attm = -10;
     enemy->def = 10;
     enemy->defm = -10;
     enemy->vel = 0;
-    printf("seleccionada la raza enana");
     } else if (enemy->race==3){       //orco
     enemy->att = 20;
     enemy->attm = -100;
     enemy->def = 0;
     enemy->defm = 0;
     enemy->vel = -20;
-    printf("seleccionada la raza orca");
     } else if (enemy->race==4){       //humano
     enemy->att = 0;
     enemy->attm = 0;
     enemy->def = 0;
     enemy->defm = 0;
     enemy->vel = 0;
-    printf("seleccionada la raza humana");
     }
-    npc pointerNPC;
     for (i=0; i<enemy->level; i++){
         levelUp(&enemy->att, &enemy->attm, &enemy->def, &enemy->defm, &enemy->vel, &enemy->race);
     }
+     
+    itemGen(&enemy->weapons[0], &enemy->level, 1, 0);
+    itemGen(&enemy->weapons[1], &enemy->level, 1, 0);
+    itemGen(&enemy->gear[0], &enemy->level, 2, 1);
+    itemGen(&enemy->gear[1], &enemy->level, 2, 2);
+    itemGen(&enemy->gear[2], &enemy->level, 2, 3);
+    itemGen(&enemy->gear[3], &enemy->level, 2, 4);
 }
 
