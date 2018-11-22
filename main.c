6c30a34ff754105b5e2918faea618fa898649b44
Enmanuel Abreu <44638604+Brodluck@users.noreply.github.com>
2018-11-22 15:07:08 +0100

    Update main.c

--- main.c
+++ main.c
@@ -5,16 +5,27 @@
 
 int main() {
     chara charac;
+    char tuto;
     int strt;
-    strt=menu();
-    
-    if (strt==0){
+    strt = menu();
+
+    if (strt == 0) {
         return 0;
-    }else {
-        printf("bienvenido al rpg game\n");
-        printf("cree su personaje\n\n");
+    } else {
+        printf("Bienvenido al rpg game\n");
+        printf("Cree su personaje\n\n");
         character(&charac);
     }
+
+        printf("¿Quieres hacer un tutorial? [Y/N]");
+        scanf("\n%c", &tuto);
+        if (tuto=='Y' || tuto=='y'){
+            //tutorial();
+        }else if (tuto=='N' || tuto=='n'){
+            printf("Bien, eres de los que aprenden sobre la marcha");
+        }
+        
+
+    
     return 0;
 }
-
